f1dd2393f983745978da93494b0a2088ffe35064
Rsk5720 <40858286+Rsk5720@users.noreply.github.com>
2018-07-25 18:16:10 +0530

    Add files via upload

--- pascalstrianglefinal.c
+++ pascalstrianglefinal.c
@@ -5,7 +5,7 @@ int main(void)
     puts("Enter the no. of lines you want to display : ");
     int len1,len;scanf("%d",&len1);
     len = len1+1;
-    int b=4*len;
+    int b=4*len-4;
     for(register int i=1;i<=len;i++)
     {
         for(register int j=0;j<b;j++)
